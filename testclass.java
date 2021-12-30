307106e3ae78a0c93cba0b9e43698aa0b915bd93
praveensatti <gitVirgo#19>
2021-12-30 22:51:25 +0530

    sub class added

--- testclass.java
+++ testclass.java
@@ -1,6 +1,18 @@
-public class testclass {
+
+class testclass {
 
     public static void main(String[] args) {
-        System.out.println("helow world !");
+        System.out.println("hello world !");
+
+        omega a = new omega();
+        int c = a.omega1(5);
+        System.out.println(c);
+    }
+}
+
+  class omega{
+    final int omega1(int a){
+        return Math.max(a,6);
+
     }
 }
